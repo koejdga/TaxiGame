b9f355dcbde77ea7f2e974fb4d75c726b3d90c10
koejdga <avolidub@gmail.com>
2022-06-26 17:10:01 +0300

    Add city assets and simple following camera

---
+++ TaxiGame/Assets/Scripts/FollowingCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    public float smoothSpeed;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+        transform.rotation = Quaternion.Lerp(transform.rotation, player.rotation, smoothSpeed);
+
+        // transform.LookAt(player);
+
+
+    }
+}
